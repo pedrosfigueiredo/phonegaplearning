2d4de98fd8e0647807d934eefef4ee2f71120a3a
Pedro Figueiredo <pedro.figueiredo@doitsistemas.com.br>
2015-08-24 16:58:23 -0300

    Adding contact integration via Contacts API

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -2,11 +2,14 @@ var EmployeeView = function(employee) {
     this.initialize = function() {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
+
     this.render = function() {
         this.el.html(EmployeeView.template(employee));
         return this;
     };
+
     this.addLocation = function(event) {
         event.preventDefault();
         console.log('addLocation');
@@ -17,6 +20,23 @@ var EmployeeView = function(employee) {
             function() {
                 alert('Error getting location');
             });
+        return false;
+    };
+
+    this.addToContacts = function(event) {
+        event.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
         return false;
     };
 
