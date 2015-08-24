2446c6bf7790bd165105c02cfd01949853a85b46
Pedro Figueiredo <pedro.figueiredo@doitsistemas.com.br>
2015-08-24 14:23:34 -0300

    Using Handlebars templates to insert content via JS

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
 var app;
 app = {
 
-    findByName: function () {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
@@ -19,6 +13,8 @@ app = {
         this.store = new MemoryStore(function () {
             self.renderHomeView();
         });
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
@@ -30,13 +26,8 @@ app = {
         }
     },
 
-    renderHomeView: function(){
-      var html = "<div class='header'><h1>Home</h1></div>" +
-              "<div class='search-view'>" +
-              "<input class='search-key'/>" +
-              "<ul class='employee-list'></ul>" +
-              "</div>";
-        $('body').html(html);
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
